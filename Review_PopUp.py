e258a5a7c2cd5ced581cc66272febbc7bcf0d8c0
noobj2 <63994740+noobj2@users.noreply.github.com>
2020-05-23 11:09:12 +0430

    Update Review_PopUp.py
    Fixed not showing the popup in full screen mode.
    minor changes

--- Review_PopUp.py
+++ Review_PopUp.py
@@ -1,11 +1,12 @@
 #// auth_ Mohamad Janati
 #// AmirHassan Asvadi ;)
-#// Copyright (c) 2019-2020 Mohamad Janati (freaking stupid, right? :|)
+#// Copyright (c) 2020 Mohamad Janati (freaking stupid, right? :|)
 
 
 from anki.hooks import wrap
 from aqt.reviewer import Reviewer
 import aqt
+from aqt import mw
 from aqt.qt import *
 import os
 from os.path import join, dirname
@@ -18,14 +19,18 @@ def myPopUp(self, ease):
     if show_random == 0:
         showImage()
 def showImage():
+    title_list = ['Youre Doing Great', 'Good Job', 'Keep Going', 'You\'re the Best']
+    button_list = ['I\'m Gonna Finish Them', 'Reviews Are Easy', 'I Enjoy Doing My Reviews', 'I Love Anki', 'I\'m not Gonna let Anything Distract me', 'I\'m Gonna Stay Focused', 'just a Few Reviews left, Finishing Them is The Easiest Thing Ever']
+    title_text = random.choice(title_list)
+    button_text = random.choice(button_list)
     image_list = ['image1.png', 'image2.png', 'image3.png', 'image4.png', 'image5.png']
     image_path = '/{}'.format(random.choice(image_list))
-    window = QDialog()
-    window.setWindowTitle("Review Pop Up")
+    window = QDialog(mw)
+    window.setWindowTitle(title_text)
     window.setWindowIcon(QIcon(images + "/icon.png"))
     picture = QLabel()
     picture.setText("<img src='{}' style='max-height:450px; max-width:450px;'>".format(images + image_path))
-    button = QPushButton('Ok')
+    button = QPushButton(button_text)
     button.clicked.connect(lambda: window.hide())
     layout = QVBoxLayout()
     layout.addWidget(picture)

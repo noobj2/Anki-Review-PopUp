f8642bfdbb175af26913f662661328761a0d4b33
noobj2 <63994740+noobj2@users.noreply.github.com>
2020-05-28 00:02:20 +0430

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,6 +1,7 @@
 #// auth_ Mohamad Janati
 #// AmirHassan Asvadi ;)
-#// Copyright (c) 2019-2020 Mohamad Janati (freaking stupid, right? :|)
+#// Copyright (c) 2020 Mohamad Janati (freaking stupid, right? :|)
 
 
 from . import Review_PopUp
+from . import Settings2

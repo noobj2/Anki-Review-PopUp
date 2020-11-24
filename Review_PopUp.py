72d4ed4ab70fb4ad33e4d3f43885440cf0e5c93b
noobj2 <63994740+noobj2@users.noreply.github.com>
2020-11-24 16:44:36 +0330

    Update Review_PopUp.py

--- Review_PopUp.py
+++ Review_PopUp.py
@@ -1,5 +1,4 @@
 #// auth_ Mohamad Janati
-#// AmirHassan Asvadi ;)
 #// Copyright (c) 2020 Mohamad Janati (freaking stupid, right? :|)
 
 
@@ -7,7 +6,7 @@ from anki.hooks import wrap
 from anki.sound import play, clearAudioQueue, AVPlayer
 from aqt.reviewer import Reviewer
 import aqt
-from aqt import mw
+from aqt import mw, gui_hooks
 from aqt.qt import *
 import os
 from os.path import join, dirname
@@ -24,19 +23,28 @@ def _play_tags(self, tags):
 
 
 def myPopUp(self, ease):
+    cnt = self.mw.col.sched.answerButtons(self.card)
     popUp_Chance = config["Pop-Up Chance"]
     show_random = random.choice(range((101 - popUp_Chance)))
     if show_random == 0:
         play_audio = config["Play Audio"]
         if self.state == "answer":
-            if ease == 1:
-                folder = 'again'
-            elif ease == 2:
-                folder = 'hard'
-            elif ease == 3:
-                folder = 'good'
+            if cnt == 3:
+                if ease == 1:
+                    folder = 'again'
+                elif ease == 2:
+                    folder = 'good'
+                else:
+                    folder = 'easy'
             else:
-                folder = 'easy'
+                if ease == 1:
+                    folder = 'again'
+                elif ease == 2:
+                    folder = 'hard'
+                elif ease == 3:
+                    folder = 'good'
+                else:
+                    folder = 'easy'
             audio_folder = join(addon_path, 'user_files/audio_video', folder)
             audioName_list = os.listdir(audio_folder)
             audio_name = '/{}'.format(random.choice(audioName_list))
@@ -45,9 +53,9 @@ def myPopUp(self, ease):
                 AVPlayer.play_tags=_play_tags
                 clearAudioQueue()
                 play(audio_path)
-            show_popUp(ease)
+            show_popUp(cnt, ease)
 
-def show_popUp(ease):
+def show_popUp(cnt, ease):
     headerText_fontSize = config["Header Text Font Size"]
     headerText_fontStyle = config["Header Text Font Style"]
     show_header = config["Show Header"]
@@ -64,31 +72,53 @@ def show_popUp(ease):
     button_list_hard = config["Button Texts_ Hard"]
     button_list_good = config["Button Texts_ Good"]
     button_list_easy = config["Button Texts_ Easy"]
-    if ease == 1:
-        folder = 'again'
-        header_text = random.choice(header_list_again)
-        title_text = random.choice(title_list_again)
-        button_text = random.choice(button_list_again)
-    elif ease == 2:
-        folder = 'hard'
-        header_text = random.choice(header_list_hard)
-        title_text = random.choice(title_list_hard)
-        button_text = random.choice(button_list_hard)
-    elif ease == 3:
-        folder = 'good'
-        header_text = random.choice(header_list_good)
-        title_text = random.choice(title_list_good)
-        button_text = random.choice(button_list_good)
-    elif ease == 4:
-        folder = 'easy'
-        header_text = random.choice(header_list_easy)
-        title_text = random.choice(title_list_easy)
-        button_text = random.choice(button_list_easy)
+    if cnt == 3:
+        if ease == 1:
+            folder = 'again'
+            header_text = random.choice(header_list_again)
+            title_text = random.choice(title_list_again)
+            button_text = random.choice(button_list_again)
+        elif ease == 2:
+            folder = 'good'
+            header_text = random.choice(header_list_good)
+            title_text = random.choice(title_list_good)
+            button_text = random.choice(button_list_good)
+        elif ease == 3:
+            folder = 'easy'
+            header_text = random.choice(header_list_easy)
+            title_text = random.choice(title_list_easy)
+            button_text = random.choice(button_list_easy)
+        else:
+            folder = 'again'
+            header_text = "cnt: {} | ease: {}".format(cnt, ease)
+            title_text = "Wrong Ease."
+            button_text = "Ok"
     else:
-        folder = 'again'
-        header_text = "Wrong Ease?"
-        title_text = "Wrong Ease."
-        button_text = "Ok"
+        if ease == 1:
+            folder = 'again'
+            header_text = random.choice(header_list_again)
+            title_text = random.choice(title_list_again)
+            button_text = random.choice(button_list_again)
+        elif ease == 2:
+            folder = 'hard'
+            header_text = random.choice(header_list_hard)
+            title_text = random.choice(title_list_hard)
+            button_text = random.choice(button_list_hard)
+        elif ease == 3:
+            folder = 'good'
+            header_text = random.choice(header_list_good)
+            title_text = random.choice(title_list_good)
+            button_text = random.choice(button_list_good)
+        elif ease == 4:
+            folder = 'easy'
+            header_text = random.choice(header_list_easy)
+            title_text = random.choice(title_list_easy)
+            button_text = random.choice(button_list_easy)
+        else:
+            folder = 'again'
+            header_text = "cnt: {} | ease: {}".format(cnt, ease)
+            title_text = "Wrong Ease."
+            button_text = "Ok"
     image_folder = join(addon_path, 'user_files/images', folder)
     imageName_list = os.listdir(image_folder)
     image_name = '/{}'.format(random.choice(imageName_list))
